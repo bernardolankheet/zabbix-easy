4a7943341dfad2b6cddd1a9a4ece43c64b832e93
Bernardo Lankheet <bernardo.lankheet_jlcptecnologia@prestador.globo>
2026-03-21 16:19:27 -0300

    correcao nextsub para zabbix proxy

--- app/cmd/app/main.go
+++ app/cmd/app/main.go
@@ -4028,6 +4028,7 @@ fetch('/locales/'+(_lang||'pt_BR')+'/messages.json?cb='+Date.now()).then(functio
 	// --- Seção: Segurança (conta Admin padrão) ---
 	if hasDefaultAdmin {
 		secNum++
+		secSub := 0
 		// badge: warn by default, escalate to crit when default password is accepted
 		badgeClass := "warn"
 		badgeIcon := "🟡"
@@ -4048,7 +4049,7 @@ fetch('/locales/'+(_lang||'pt_BR')+'/messages.json?cb='+Date.now()).then(functio
 			`</div><span class='status-badge ` + badgeClass + `'>` + badgeIcon + `</span>` +
 			`<span class='rec-sec-arrow'>▶</span></summary>` +
 			`<div class='rec-sec-body'>` +
-			`<h5>` + nextSub() + ` <span data-i18n='sub.default_admin_account'></span></h5>` +
+			`<h5>` + nextSub(&secSub, "") + ` <span data-i18n='sub.default_admin_account'></span></h5>` +
 			`<p data-i18n='tip.default_admin'></p>` +
 			`<div class='fix-box'><div class='fix-box-title'>🔧 <span data-i18n='fix.how_to_resolve'></span></div>` +
 			`<ul>` +
